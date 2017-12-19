9e2cb593ea50f680ee3aa90c05f8c17ee8f143dc
F <f@s.o>
2017-12-19 14:12:36 +0100

    implement remove todo

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+
 
 @Component({
   selector: 'app-todolist',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
+  inputText: string;
+  selectedIndex: number;
+
+  todos: string[] = [];
+  isShowJoke = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.todos.push('foo');
+    this.todos.push('bar');
+    this.isShowJoke = false;
+  }
+
+  selectTodo(index: number) {
+    console.log('select #' + index);
+    this.selectedIndex = index;
+    this.inputText = this.todos[index];
+  }
+
+  removeTodo(index: number) {
+    console.log('removing #' + index);
+    this.todos.splice(index, 1);
+    this.isShowJoke = true;
+  }
+
+  addTodo(text: string) {
+    console.log('add new todo ' + text);
+    this.todos.push(text);
+    this.isShowJoke = false;
   }
 
 }
